a3fdf562fcbf6b50265218332e3204b5e1d00136
Jorge Villada <jorge-1409@hotmail.com>
2017-04-09 18:17:25 -0500

    Updating dist

--- dist/form-viewer.js
+++ dist/form-viewer.js
@@ -275,12 +275,16 @@ angular.module('mwFormViewer').directive('mwFormViewer', ["$rootScope", function
                 }
             };
             
-            ctrl.resetPages = function(){
+            ctrl._resetPages = function(){
                 ctrl.prevPages=[];
 
                 ctrl.currentPage=null;
                 ctrl.nextPage = null;
                 ctrl.formSubmitted=false;
+            };
+            
+            ctrl.resetPages = function(){
+                ctrl._resetPages();
                 if(ctrl.options.autoStart){
                     ctrl.beginResponse();
                 }
@@ -363,7 +367,7 @@ angular.module('mwFormViewer').directive('mwFormViewer', ["$rootScope", function
             
             scope.$on('mwForm.pageEvents.changePage', function(event,data){
                 if(typeof data.page !== "undefined" && data.page < ctrl.formData.pages.length){
-                   ctrl.resetPages();
+                   ctrl._resetPages();
                    for(var i =0; i < data.page;i++){
                         ctrl.prevPages.push(ctrl.formData.pages[i]);
                    } 
